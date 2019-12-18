8f9237b078772bf9be2ac9fd2cd3e9e4a0c6a585
apple <appleapple@apples-MacBook-Air.local>
2019-12-18 18:47:01 +0530

    db set to postgres

--- Gymnasium/settings.py
+++ Gymnasium/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env= environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -84,10 +86,11 @@ WSGI_APPLICATION = 'Gymnasium.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': env.db('DATABASE_URL', default='postgres:///gymnasium_db')
+    #{
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 }
 
 
