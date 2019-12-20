efa6bb881a10530a6aaa47f3ee57c591cf7b9e68
apple <appleapple@apples-MacBook-Air.local>
2019-12-20 14:03:36 +0530

    corrected STATIC_ROOT dir

--- Gymnasium/settings.py
+++ Gymnasium/settings.py
@@ -133,8 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+#print ('dir_name path:', PROJECT_DIR)
+STATIC_ROOT = BASE_DIR
+print ('static_root:',STATIC_ROOT)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
