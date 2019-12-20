9612efbc93a9764f7b7f32804b0e966f784a7d0b
apple <appleapple@apples-MacBook-Air.local>
2019-12-20 15:39:20 +0530

    settings file static root changed

--- Gymnasium/settings.py
+++ Gymnasium/settings.py
@@ -132,15 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 #print ('dir_name path:', PROJECT_DIR)
-STATIC_ROOT =os.path.join( BASE_DIR,'static')
-print ('static_root:',STATIC_ROOT)
+#print ('static_root:',STATIC_ROOT)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
