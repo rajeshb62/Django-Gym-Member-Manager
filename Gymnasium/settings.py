f7c62f2af76c125b55f4b1772ab443131bfcfb10
apple <appleapple@apples-MacBook-Air.local>
2019-12-20 16:49:09 +0530

    static dir changed in settings

--- Gymnasium/settings.py
+++ Gymnasium/settings.py
@@ -139,7 +139,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
